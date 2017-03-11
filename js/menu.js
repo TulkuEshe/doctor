e339bcf9badeb74e7931fa489b7ec2066d3c42a5
Alex <eshedrug@gmail.com>
2017-03-11 17:56:38 +0200

    feat: popup styling

--- js/menu.js
+++ js/menu.js
@@ -1,8 +1,10 @@
 $(function() {
   $('.hamburger').click(function() {
     $('.header').addClass('header--active');
+    $('.body').addClass('body--no-overflow');
   });
   $('.header').click(function() {
     $('.header').removeClass('header--active');
+    $('.body').removeClass('body--no-overflow');
   });
 });
